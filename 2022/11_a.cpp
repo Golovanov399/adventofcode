c33e3b31bd6c5d81d5085cb55e2cd2cbf661ce38
Alexander Golovanov <Golovanov12345@gmail.com>
2022-12-11 08:41:30 +0300

    Add day 11

--- 2022/11_a.cpp
+++ 2022/11_a.cpp
@@ -36,7 +36,7 @@ struct Monkey {
 	int to[2];
 };
 
-long long f(string op, int x) {
+int f(string op, int x) {
 	auto tkns = split(op, " ");
 	for (auto& s : tkns) {
 		if (s == "old") {
@@ -47,8 +47,8 @@ long long f(string op, int x) {
 		if (tkns[i] != "*") {
 			continue;
 		}
-		auto x = stoll(tkns[i - 1]);
-		auto y = stoll(tkns[i + 1]);
+		int x = stoi(tkns[i - 1]);
+		int y = stoi(tkns[i + 1]);
 		tkns.erase(tkns.begin() + i);
 		tkns.erase(tkns.begin() + i);
 		tkns[i - 1] = to_string(x * y);
@@ -60,15 +60,15 @@ long long f(string op, int x) {
 		}
 		assert(tkns[i] == "+" || tkns[i] == "-");
 		char c = tkns[i][0];
-		auto x = stoll(tkns[i - 1]);
-		auto y = stoll(tkns[i + 1]);
+		int x = stoi(tkns[i - 1]);
+		int y = stoi(tkns[i + 1]);
 		tkns.erase(tkns.begin() + i);
 		tkns.erase(tkns.begin() + i);
 		tkns[i - 1] = to_string(c == '+' ? x + y : x - y);
 		i -= 1;
 	}
 	assert((int)tkns.size() == 1);
-	return stoll(tkns[0]);
+	return stoi(tkns[0]);
 }
 
 int main() {
@@ -112,28 +112,22 @@ int main() {
 		}
 	}
 
-	int lc = 1;
-	for (auto mon : a) {
-		lc = lcm(lc, mon.div_by);
-	}
-	cerr << lc << "\n";
-
 	const int n = a.size();
 	vector<int> cnt(n);
+	vector<int> sum(n);
 
-	for (int it = 0; it < 10000; ++it) {
+	for (int it = 0; it < 20; ++it) {
 		for (int i = 0; i < n; ++i) {
 			auto& mon = a[i];
-			for (long long x : mon.items) {
+			for (int x : mon.items) {
 				cnt[i] += 1;
+				sum[i] += x;
 				// cerr << "Monkey " << i << " has item " << x << "\n";
 				x = f(mon.op, x);
 				// cerr << "Now it is " << x << "\n";
-				// assert(x >= 0);
-				x %= lc;
-				if (x < 0) {
-					x += lc;
-				}
+				assert(x >= 0);
+				x /= 3;
+					// cerr << "Test passed, monkey " << mon.to[1] << " acquires item " << x << "\n";
 				a[mon.to[x % mon.div_by == 0]].items.push_back(x);
 			}
 			mon.items = {};
@@ -149,7 +143,7 @@ int main() {
 	{
 		auto vec = cnt;
 		sort(all(vec));
-		cout << 1ll * vec[n - 2] * vec[n - 1] << "\n";
+		cout << vec[n - 2] * vec[n - 1] << "\n";
 	}
 	// for (auto x : cnt) {
 	// 	cerr << x << " ";
