7d33e26737a09dd997899e7f3d6ace758e82843d
Alexander Golovanov <Golovanov12345@gmail.com>
2023-12-06 02:05:54 +0300

    Shorten util.kt

--- 2023/util.kt
+++ 2023/util.kt
@@ -1,33 +1,5 @@
-fun <T> List<T>.split(x: T): List<List<T>> {
-    val cur = mutableListOf<T>()
-    val res = mutableListOf<List<T>>()
-    for (y in this) {
-        if (y == x) {
-            res.add(cur.toList())
-            cur.clear()
-        } else {
-            cur.add(y)
-        }
-    }
-    if (cur.isNotEmpty()) {
-        res.add(cur.toList())
-    }
-    return res
-}
+fun <T> List<T>.split(predicate: (T) -> Boolean) = (
+        listOf(-1) + this.withIndex().filter { predicate(it.value) }.map { it.index} + listOf(this.size)
+    ).zipWithNext().map { this.subList(it.first + 1, it.second) }
 
-fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> {
-    val cur = mutableListOf<T>()
-    val res = mutableListOf<List<T>>()
-    for (y in this) {
-        if (predicate(y)) {
-            res.add(cur.toList())
-            cur.clear()
-        } else {
-            cur.add(y)
-        }
-    }
-    if (cur.isNotEmpty()) {
-        res.add(cur.toList())
-    }
-    return res
-}
+fun <T> List<T>.split(x: T) = this.split { it == x }
