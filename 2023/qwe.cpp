fab74efce1c163839e1c13e372ea68efe9efe555
Alexander Golovanov <Golovanov12345@gmail.com>
2023-12-06 23:21:08 +0300

    Minor qwe.cpp change

--- 2023/qwe.cpp
+++ 2023/qwe.cpp
@@ -11,21 +11,9 @@ inline int nxt() {
 	return x;
 }
 
-int best(List<String> a, bool mcb) {
-	const int sz = a[0].length();
-	return range(sz).fold(a, [&](auto list, int i) {
-		if (list.size() == 1) {
-			return list;
-		}
-		auto cnt = range(2).map([&](int c) { return list.count([&](auto s) { return s[i] == '0' + c; }); });
-		const int bit = '0' + (cnt[0] <= cnt[1]);
-		return list.filter([&](auto s) { return (s[i] == bit) == mcb; });
-	})[0].reversed().with_index().sum_of([](const auto& iv) { return ((int)iv.value - '0') << iv.index; });
-}
-
 int main() {
 	auto a = read_strings();
-	cout << best(a, true) * best(a, false) << "\n";
+	//
 
 	return 0;
 }
