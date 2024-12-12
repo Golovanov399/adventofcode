904f71e85a216f57c007e2034f2dd54d78919ea3
Alexander Golovanov <Golovanov12345@gmail.com>
2024-12-12 08:22:48 +0300

    Add day 12

--- 2024/12_a.cpp
+++ 2024/12_a.cpp
@@ -56,13 +56,6 @@ int main() {
 	const int n = a.size();
 	const int m = a[0].size();
 	vector<vector<char>> used(n, vector<char>(m, 0));
-
-	auto add_side = [&](set<array<int, 4>>& S, int x, int y, int nx, int ny) {
-		int dx = nx - x, dy = ny - y;
-		int sx = x + nx + 1, sy = y + ny + 1;
-		S.insert({sx - dy, sy - dx, sx + dy, sy + dx});
-	};
-
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
 			if (used[i][j]) {
@@ -71,7 +64,7 @@ int main() {
 			vector<pair<int, int>> st = {{i, j}};
 			used[i][j] = true;
 			int ver = 0;
-			set<array<int, 4>> sides;
+			int ed = 0;
 			while (!st.empty()) {
 				auto [x, y] = st.back();
 				st.pop_back();
@@ -83,12 +76,13 @@ int main() {
 						}
 						int nx = x + dx, ny = y + dy;
 						if (clamp(nx, 0, n - 1) != nx || clamp(ny, 0, m - 1) != ny) {
-							add_side(sides, x, y, nx, ny);
 							continue;
 						}
 						if (a[nx][ny] != a[x][y]) {
-							add_side(sides, x, y, nx, ny);
-						} else if (!used[nx][ny]) {
+							continue;
+						}
+						++ed;
+						if (!used[nx][ny]) {
 							st.push_back({nx, ny});
 							used[nx][ny] = true;
 						}
@@ -96,22 +90,7 @@ int main() {
 				}
 			}
 
-			map<pair<int, int>, pair<int, int>> M;
-			map<pair<int, int>, int> cnt;
-			for (auto v : sides) {
-				M[{v[0], v[1]}].first += v[2];
-				M[{v[0], v[1]}].second += v[3];
-				M[{v[2], v[3]}].first += v[0];
-				M[{v[2], v[3]}].second += v[1];
-				cnt[{v[0], v[1]}] += 1;
-				cnt[{v[2], v[3]}] += 1;
-			}
-			int per = 0;
-			for (auto [k, v] : M) {
-				per += v.first != k.first * 2 || v.second != k.second * 2;
-				per += cnt[k] / 2 - 1;
-			}
-			// cerr << a[i][j] << ": " << ver << " " << per << "\n";
+			int per = 4 * ver - ed;
 			ans += 1ll * ver * per;
 		}
 	}
