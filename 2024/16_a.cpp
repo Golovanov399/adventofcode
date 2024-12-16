3f7996e337d9ca292aaa39cf3aada2a267d689fc
Alexander Golovanov <Golovanov12345@gmail.com>
2024-12-16 08:18:11 +0300

    Add day 16

--- 2024/16_a.cpp
+++ 2024/16_a.cpp
@@ -59,32 +59,24 @@ int main() {
 	const int n = a.size(), m = a[0].size();
 
 	pair<int, int> start = {-1, -1};
-	pair<int, int> finish = {-1, -1};
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < m; ++j) {
 			if (a[i][j] == 'S') {
 				start = {i, j};
 			}
-			if (a[i][j] == 'E') {
-				finish = {i, j};
-			}
 		}
 	}
 	vector<int> dp(n * m * 4, inf);
-	vector<vector<int>> prv(n * m * 4);
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-	auto upd = [&](int from, int x, int y, int d, int val) {
+	auto upd = [&](int x, int y, int d, int val) {
 		const int xyd = (x * m + y) * 4 + d;
 		if (dp[xyd] > val) {
-			prv[xyd] = {from};
 			dp[xyd] = val;
 			pq.push({val, xyd});
-		} else if (dp[xyd] == val) {
-			prv[xyd].push_back(from);
 		}
 	};
-	upd(-1, start.first, start.second, 0, 0);
+	upd(start.first, start.second, 0, 0);
 
 	while (!pq.empty()) {
 		auto [val, xyd] = pq.top();
@@ -95,41 +87,17 @@ int main() {
 		int d = xyd % 4;
 		int y = xyd / 4 % m;
 		int x = xyd / 4 / m;
-		upd(xyd, x, y, (d + 1) % 4, val + 1000);
-		upd(xyd, x, y, (d + 3) % 4, val + 1000);
+		if (a[x][y] == 'E') {
+			cout << val << "\n";
+			break;
+		}
+		upd(x, y, (d + 1) % 4, val + 1000);
+		upd(x, y, (d + 3) % 4, val + 1000);
 		int nx = x + dx[d], ny = y + dy[d];
 		if (a[nx][ny] != '#') {
-			upd(xyd, nx, ny, d, val + 1);
+			upd(nx, ny, d, val + 1);
 		}
 	}
-
-	set<int> best;
-	set<int> was;
-	{
-		int res = inf;
-		for (int i = 0; i < 4; ++i) {
-			res = min(res, dp[(finish.first * m + finish.second) * 4 + i]);
-		}
-		for (int i = 0; i < 4; ++i) {
-			int cand = (finish.first * m + finish.second) * 4 + i;
-			if (res == dp[cand]) {
-				best.insert(cand);
-			}
-		}
-	}
-	set<int> res;
-	while (!best.empty()) {
-		int v = *best.begin();
-		best.erase(best.begin());
-		was.insert(v);
-		res.insert(v / 4);
-		for (int x : prv[v]) {
-			if (x != -1 && !was.contains(x)) {
-				best.insert(x);
-			}
-		}
-	}
-	cout << res.size() << "\n";
 
 	return 0;
 }
